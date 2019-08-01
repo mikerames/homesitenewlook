c3ba378505ad188a64aa07d24a7c85a0c360d477
Miguel Ramos <miguel.ramos@msn.com>
2019-08-01 17:37:49 +0100

    Add files via upload

---
+++ deck_of_cards.js
@@ -0,0 +1,52 @@
+class Deck {
+  constructor() {
+    this.deck = [];
+
+    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+    for (let suit in suits) {
+      for (let value in values) {
+        this.deck.push(`${values[value]} of ${suits[suit]}`);
+      }
+    }
+  }
+}
+
+/*
+const deck1 = new Deck();
+console.log(deck1.deck);
+// ["Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades", "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"]
+
+shuffle() {
+  const { deck } = this;
+  let m = deck.length, i;
+
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+
+    [deck[m], deck[i]] = [deck[i], deck[m]];
+  }
+
+  return this;
+}
+
+const { deck } = this;
+const deck = this.deck;
+
+// Option 1
+let m = deck.length;
+let i;
+
+// Option 2
+let me = deck.length,
+  i;
+
+// Option 3
+let m = deck.length, i;
+
+deal() {
+  return this.deck.pop();
+}
+
+*/
--- script.js
+++ script.js
@@ -1,56 +1,54 @@
+    // API 
+    let url = 'http://nodejstodomike.herokuapp.com/api/todos';
 
-let portfPhotos = [{
-    name: 'Mountains',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/mountains.jpg',
-    alt: 'Mountains',
-    category: 'bodyboard'
-}, {
-    name: 'Lights',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/lights.jpg',
-    alt: 'Lights',
-    category: 'people'
-}, {
-    name: 'Forest',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/nature.jpg',
-    alt: 'Forest',
-    category: 'bodyboard'
-}, {
-    name: 'Cars',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/cars1.jpg',
-    alt: 'Cars',
-    category: 'motocross'
-}, {
-    name: 'Mountains',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/mountains.jpg',
-    alt: 'Mountains',
-    category: 'motocross'
-}, {
-    name: 'Lights',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/lights.jpg',
-    alt: 'Lights',
-    category: 'motocross'
-}, {
-    name: 'Forest',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/nature.jpg',
-    alt: 'Forest',
-    category: 'cars'
-}, {
-    name: 'Cars',
-    description: 'Lorem ipsum dolor..',
-    imgUrl: 'https://www.w3schools.com//w3images/cars1.jpg',
-    alt: 'Cars',
-    category: 'nature'
-}]
+    let portfPhotos = [];
+    let portfCategories = [];
+    let portfBtnFiltersTemplate;
+    let portfolioTemplate;
+    let profileCardTemplate;    
 
+    let profilePhotos = [{
+        alt: "avatar",
+        imgUrl: "https://www.w3schools.com/howto/img_avatar.png",
+        imgUrlBack: "https://www.w3schools.com//w3images/lights.jpg"
+    }, {
+        alt: "avatar",
+        imgUrl: "https://www.w3schools.com/howto/img_avatar.png",
+        imgUrlBack: "https://www.w3schools.com//w3images/lights.jpg"
+    }]
 
-
+    function filterSelection(c) {
+        var x, i;
+        x = document.getElementsByClassName("column");
+        if (c == "all") c = "";
+        for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        }
+    }
+    
+    // adds show class to element which consists in display: block
+    function w3AddClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+        }
+    }
+    
+    function w3RemoveClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);     
+        }
+        }
+        element.className = arr1.join(" ");
+    }  
+   
 
 $(document).ready(function() {
 
@@ -74,9 +72,26 @@ $(document).ready(function() {
         }, 500);
     });
 
-    // Portfolio template
-    let porttfolioTemplate = _.template(
-        "<div class='column <%= category %>'>" +
+    $("#portfolio-section").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#myBtnContainer").offset().top
+        }, 500);
+    });
+
+
+
+
+
+    // TEMPLATES
+    
+    // Button Filters
+    portfBtnFiltersTemplate = _.template(
+        "<button class='btn' onclick='filterSelection(\"\<%= portfCategories %>\"\)'><%= portfCategories %></button>"
+    );
+    
+     // Portfolio
+     portfolioTemplate = _.template(
+        "<div class='column <%= category %> show'>" +
         "<div class='content'>" +
         "<img src='<%= imgUrl %>' alt='<%= alt %>' style='width:100%' onclick='myFunction(this);'>" +
         "<h4><%= name %></h4>" +
@@ -85,18 +100,81 @@ $(document).ready(function() {
         "</div>"
     );
 
-    // Append portfolio data to template
-    portfPhotos.forEach(function(photo) {
-        $(".portfolio").append(porttfolioTemplate({
-            name: photo.name,
-            description: photo.description,
-            imgUrl: photo.imgUrl,
-            alt: photo.alt,
-            category: photo.category
-        }));
-    });
 
-    // Show all photos in portfolio section by activating filter for all
-    filterSelection('all');
+    // Flip Card
+    profileCardTemplate = _.template(
+        "<div class='column show' style='width: 160px'>" +
+        "<div class='flip-card'>" +
+        "<div class='flip-card-inner'>" +
+        "<div class='flip-card-front'>" +
+        "<img src='<%= imgUrl %>' alt='<%= alt %>' style='width:150px;height:200px;margin: 0;'>" +
+        "</div>" +
+        "<div class='flip-card-back'>" +
+        "<img src='<%= imgUrlBack %>' alt='<%= alt %>' style='width:150px;height:200px;margin: 0;'>" +
+        "</div>" +
+        "</div>" +
+        "<div>" +
+        "</div>"
+    );
+
+    
+    // Append portfolio data to template  
+    profilePhotos.forEach(function(photo) {
+        $(".profile").append(profileCardTemplate({
+            imgUrl: photo.imgUrl,
+            imgUrlBack: photo.imgUrlBack,
+            alt: photo.alt
+        }));
+    })
+
+ 
+
+    // Creates the portfolio section with data retrieved
+    function createPortfolioSection(photo) {
+        photo.forEach(function(photo){
+            $(".portfolio").append(portfolioTemplate({
+                name: photo.name,
+                description: photo.description,
+                imgUrl: photo.imgUrl,
+                alt: photo.alt,
+                category: photo.category
+            }));
+            // add to category if not exist yet
+            if(portfCategories.indexOf(photo.category) === -1) {
+                portfCategories.push(photo.category);
+            };
+        });        
+        portfCategories.forEach(function(btn) {
+            $(".myBtnFilter").append(portfBtnFiltersTemplate({
+                portfCategories: btn
+            }));
+        });
+        $(".myBtnFilter .btn").first().addClass("active");
+        
+        // Add active class to the current button (highlight it)
+        let btnContainer = document.getElementById("myBtnContainer");
+        let btns = document.getElementsByClassName("btn");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function() {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+            });
+        }
+
+    }
+
+
+    // REQUEST TO NODEJS
+    $.get(url)
+    .done(
+        function(response) {
+            portfCategories.push('all'),
+            createPortfolioSection(response);          
+        }
+    );
+
+        
+
 
 });
